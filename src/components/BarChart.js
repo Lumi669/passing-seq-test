c2b26ad82032702a6488ef7f40ca09145966e5f5
cmjiwa <wangjinghuan@yahoo.com>
2022-08-11 19:27:22 +0300

    update dependency and still works

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
+import { Chart as ChartJS, registerables } from "chart.js";
+import { Chart } from "react-chartjs-2";
+ChartJS.register(...registerables);
 
 const BarChart = ({ data }) => {
   return (
